42ae20c1f21b4137128389401943989476cdb08c
Mehti <102772767+Mehtisup@users.noreply.github.com>
2022-06-05 02:59:26 +0430

    Create login.php

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width , initial-scale=1">
+<style>
+
+</style>
+</head>
+<body>
+
+<form action="show.php" method="post">
+    <label for="username">Username</label><br>
+    <input type="text" name="username"><br>
+    <label for="password">Password</label><br>
+    <input type="text" name="password"><br>
+    <label for="age">Age</label><br>
+    <input type="text" name="age"><br>
+    <br>
+    <button type="submit">Submit</button>
+</form>
+
+</body>
+</html>
